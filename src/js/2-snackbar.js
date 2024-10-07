918ad2929030ffd13bae3963cfaa5abf76a62625
Anton Povar <povar.anton@gmail.com>
2024-10-07 09:28:04 +0300

    Add snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delayInput = event.target.elements.delay;
+  const stateInput = event.target.elements.state;
+
+  const delay = parseInt(delayInput.value, 10);
+  const state = stateInput.value;
+
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
